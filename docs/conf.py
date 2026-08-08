8413d962a44aff008d618e08c21b5d24e51112b8
CIENA\okrarup <okrarup@ciena.com>
2026-08-08 17:48:07 +0200

    Removed reference to local directory

--- docs/conf.py
+++ docs/conf.py
@@ -5,10 +5,9 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-import os
-import sys
-sys.path.insert(0,os.path.abspath(".."))
-sys.path.insert(0,os.path.abspath("C:\\Users\\olekr\\NLSE-vector-solver\\"))
+#import os
+#import sys
+#sys.path.insert(0,os.path.abspath(".."))
 
 project = 'NLSE-solver'
 copyright = '2026, Ole Krarup'
