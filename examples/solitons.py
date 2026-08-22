c5da398abcc731b8b42790fac90399b67c742a6b
CIENA\okrarup <okrarup@ciena.com>
2026-08-22 19:36:03 +0200

    Hopefully fixed error with soliton example documentaion not being built

--- examples/solitons.py
+++ examples/solitons.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('../') #Makes sure that this script can find the ssfm code. 
-from ssfm_functions import ssfm_functions as sf
+from NLSE.ssfm_functions import *
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
@@ -20,8 +20,8 @@ def simulate_solitons():
     dt = 10e-15  # Time resolution [s]
     center_wavelength_m = 1550e-9
 
-    center_freq = sf.wavelength_to_freq(center_wavelength_m)
-    time_freq = sf.TimeFreq(number_of_points=N,
+    center_freq = wavelength_to_freq(center_wavelength_m)
+    time_freq = TimeFreq(number_of_points=N,
                                 time_step_s=dt,
                                 center_frequency_Hz=center_freq)
 
@@ -33,29 +33,29 @@ def simulate_solitons():
 
     ################ Set up fibers ################ 
     alpha_dB_per_km = 0 #dB/m
-    beta_list = [sf.BETA2_AT_1550_NM_TYPICAL_SMF_S2_PER_M]   #[s^2/m,s^3/m,...]  s^(entry+2)/m
+    beta_list = [BETA2_AT_1550_NM_TYPICAL_SMF_S2_PER_M]   #[s^2/m,s^3/m,...]  s^(entry+2)/m
     
     gamma_W_per_m =  1e-3 # 1/W/m
 
     length_m = np.pi/2*duration_s**2/np.abs(beta_list[0]) #m
     number_of_steps = 2**10
 
-    fiber = sf.FiberSpan(
+    fiber = FiberSpan(
         length_m,
         number_of_steps,
         gamma_W_per_m,
         beta_list,
         alpha_dB_per_km)
-    fiber_link = sf.FiberLink([fiber]) #The SSFM solver needs a "FiberLink" class, which is generated from a list of 1 or more fibers. 
+    fiber_link = FiberLink([fiber]) #The SSFM solver needs a "FiberLink" class, which is generated from a list of 1 or more fibers. 
 
     #Set up identical fiber with no nonlinearity for comparison
-    fiber_no_NL = sf.FiberSpan(
+    fiber_no_NL = FiberSpan(
         length_m,
         number_of_steps,
         0,
         beta_list,
         alpha_dB_per_km)
-    fiber_link_no_NL = sf.FiberLink([fiber_no_NL]) #The SSFM solver needs a "FiberLink" class, which is generated from a list of 1 or more fibers. 
+    fiber_link_no_NL = FiberLink([fiber_no_NL]) #The SSFM solver needs a "FiberLink" class, which is generated from a list of 1 or more fibers. 
 
 
     ################ Set up input signals ################ 
@@ -63,7 +63,7 @@ def simulate_solitons():
     soliton_order_2 = 2
     A_char_sqrt_W_order_2 =soliton_order_2*np.sqrt(np.abs(fiber.beta_list[0])/fiber.gamma_per_W_per_m)/duration_s  #Carefuly chosen peak field to ensure soliton propagation
 
-    input_signal_order_2= sf.InputSignal(time_freq,
+    input_signal_order_2= InputSignal(time_freq,
                    amplitude_sqrt_W=A_char_sqrt_W_order_2,
                    duration_s=duration_s,
                    pulse_type="sech",
@@ -75,7 +75,7 @@ def simulate_solitons():
     soliton_order_4 = 4
     A_char_sqrt_W_order_4 =soliton_order_4*np.sqrt(np.abs(fiber.beta_list[0])/fiber.gamma_per_W_per_m)/duration_s  #Carefuly chosen peak field to ensure soliton propagation
 
-    input_signal_order_4= sf.InputSignal(time_freq,
+    input_signal_order_4= InputSignal(time_freq,
                    amplitude_sqrt_W=A_char_sqrt_W_order_4,
                    duration_s=duration_s,
                    pulse_type="sech",
@@ -83,15 +83,15 @@ def simulate_solitons():
 
 
     ################ Run split step simulations ################ 
-    ssfm_result_list_order_2_no_NL = sf.SSFM(fiber_link=fiber_link_no_NL,
+    ssfm_result_list_order_2_no_NL = SSFM(fiber_link=fiber_link_no_NL,
                                input_signal=input_signal_order_2,
                                show_progress_flag=True)
 
-    ssfm_result_list_order_2 = sf.SSFM(fiber_link=fiber_link,
+    ssfm_result_list_order_2 = SSFM(fiber_link=fiber_link,
                                input_signal=input_signal_order_2,
                                show_progress_flag=True)
 
-    ssfm_result_list_order_4 = sf.SSFM(fiber_link=fiber_link,
+    ssfm_result_list_order_4 = SSFM(fiber_link=fiber_link,
                                input_signal=input_signal_order_4,
                                show_progress_flag=True)
 
@@ -104,21 +104,21 @@ def simulate_solitons():
 
 
     #Without nonlinearity, the initial sech pulse will simply broaden in the time domain
-    sf.plot_everything_about_result(ssfm_result_list_order_2_no_NL,
+    plot_everything_about_result(ssfm_result_list_order_2_no_NL,
                                     nrange_pulse=nrange_pulse,
                                     dB_cutoff_pulse=dB_cutoff_pulse,
                                     nrange_spectrum=nrange_spectrum,
                                     dB_cutoff_spectrum=dB_cutoff_spectrum)
 
     #The N=2 soliton oscillates once
-    sf.plot_everything_about_result(ssfm_result_list_order_2,
+    plot_everything_about_result(ssfm_result_list_order_2,
                                     nrange_pulse=nrange_pulse,
                                     dB_cutoff_pulse=dB_cutoff_pulse,
                                     nrange_spectrum=nrange_spectrum,
                                     dB_cutoff_spectrum=dB_cutoff_spectrum)
 
     #The N=4 soliton oscillates 3 times
-    sf.plot_everything_about_result(ssfm_result_list_order_4,
+    plot_everything_about_result(ssfm_result_list_order_4,
                                     nrange_pulse=nrange_pulse,
                                     dB_cutoff_pulse=dB_cutoff_pulse,
                                     nrange_spectrum=nrange_spectrum,
